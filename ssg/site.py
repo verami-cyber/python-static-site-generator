39ed262eca3611ef3bd5b7008538819056fddcf1
verami-cyber <72043993+verami-cyber@users.noreply.github.com>
2020-09-28 17:08:14 -0400

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 
+
 class Site:
     def __init__ (self, source, dest):
         self.source = Path(source)
         self.dest = Path(source)
 
     def create_dir (self, path):
-        directory = self.dest / relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build():
+    def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-        for value in self.source.rglob("*"):
+        for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
